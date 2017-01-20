ee0e6d9505c68954118c0cdf18220c1e5c09e3ab
ryuxik <smunoz@mit.edu>
2017-01-19 23:59:45 -0500

    added flush logic

--- evaluator.py
+++ evaluator.py
@@ -1,9 +1,6 @@
 def evalFlop(boardCards, holeCardOne, holeCardTwo):
 	new = [holeCardTwo, holeCardOne]
-	for card in boardCards:
-		new.append(card)
-	
-	flushChance = 0.0
+	flushChance = flush(board,holes)
 	straightChance = 0.0
 	pair = False
 	twoPair = False
@@ -11,4 +8,32 @@ def evalFlop(boardCards, holeCardOne, holeCardTwo):
 	quad = False
 	highCardVal = None
 
-	
+
+def flush(board, holes):
+	numberOfCards = len(full+holes)
+	holeSuits = [hole.suitVal for hole in holes]
+	holeSuitsLen = len(set(holeSuits)) 
+	boardSuits = [b.suitVal for b in board]
+	boardSuitsLen = len(set(boardSuits))
+	fullSuitsLen = len(set(boardSuits + holeSuits))
+	if fullSuitsLen == 1:
+		return 1.0 
+
+		#need to get this right
+	elif numberOfCards == 5:
+		if fullSuitsLen == 2 and holeSuitsLen == 1:
+			return .16
+		else:
+			return 0.0
+
+	elif numberOfCards == 6:
+		if fullSuitsLen == 2 and holeSuitsLen == 1:
+			return .08
+		else:
+			return 0.0
+	else:
+		return 0
+
+
+		
+
