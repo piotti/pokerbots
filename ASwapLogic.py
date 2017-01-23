b2de07a5027a33a4970f59f6f83f46b1ba147cc2
ryuxik <smunoz@mit.edu>
2017-01-22 19:12:16 -0500

    logic after discard on flop

--- ASwapLogic.py
+++ ASwapLogic.py
@@ -1,5 +1,11 @@
 def getAction(button, lastActions, minRaise, maxRaise, potSize, myBank, hand, board, x):
-	# rating = x.evaluate(hand, board)
+	handNums = [card.num for card in hand]
+	boardNums = [boardCard.num for boardCard in board]
+	rating = x.evaluate(handNums, boardNums)
+	score = x.get_rank_class(rating)
+	scoreReal = x.class_to_string(score)
+	if scoreReal != "High Card" or scoreReal != 'Pair':
+		print scoreReal, "good hand"
 	
 	action = "CALL\n"
 	return action
