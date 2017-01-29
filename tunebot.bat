5a18295ae76685fd91ab37e08559b293123f4e87
ryuxik <smunoz@mit.edu>
2017-01-28 21:21:31 -0500

    I think the tunebot for loop is feeding the correct numbers into the thing now but im not sure

--- tunebot.bat
+++ tunebot.bat
@@ -2,16 +2,20 @@
 setlocal enabledelayedexpansion
 
 
-for /l %%x in (0, 1, 40) do (
-	set /a num1 = %%x %% 2
+for /l %%x in (1, 1, 40)  do (
+	set /a num1 = %%x
+	set /a num1 = !num1! %% 2
 	set /a num2 = !num1! + 1
-	echo gen %%x
+	echo !num1!, !num2!
+	pause
 	start runengine.bat
-	start runtunebot.bat 3000 %%num1
-	python Player.py 3001 %%num2
+	start runtunebot.bat 3000 !num1!
+	pause
+	python Player.py 3001 !num2!
+	pause
 	python bottuner.py %%x
 	python evolution.py
-	
+	pause
 )
 del winners.txt
 
